ee35bb947e4aa72712c04e42b5d1649c700606e9
Nick Tag <nick.tagliasacchi@gmail.com>
2017-03-06 01:03:34 -0500

    Added animated pacman instead of blinking cursor.

--- js/BannerController.js
+++ js/BannerController.js
@@ -1,9 +1,19 @@
 angular.module('app').controller('BannerCtlr', function ($scope) {
-    $(function(){
+    $(function () {
         $(".typingBannerText").typed({
-          strings: ["nicktag.tech is currently under Maintenance.", "Nick Tagliasacchi is getting ready for hire!", "Nick Tagliasacchi"],
-          typeSpeed: 0.1,
-          loop: true
+            strings: ["nicktag.tech is currently under Maintenance. ", "Nick Tagliasacchi is getting ready for hire! ", "Nick Tagliasacchi "],
+            typeSpeed: 20,
+            loop: false,
+            loopCount: 2,
+            cursorChar: "<div id=\"pacman\" class=\"pacmanRight\"></div>",
+            preStringTyped: function () {
+                $("#pacman").removeClass("pacmanLeft");
+                $("#pacman").addClass("pacmanRight");
+            },
+            onStringTyped: function () {
+                $("#pacman").removeClass("pacmanRight");
+                $("#pacman").addClass("pacmanLeft");
+            }
         });
     });
 });
