8a1db09126c6dc4f00af9490d78a637067c5ddc6
Nick Tag <nick.tagliasacchi@gmail.com>
2017-03-25 00:52:28 -0400

    Added fat macro modifier slider.
    Added slider to macros page for macro fat modifier. Will allow users to
    better customize their macro breakdown. Default is set to 45%.
    
    Better organization of code in the MacroController. Changed macro grams
    calculations to rounding instead of two digit truncating.
    
    Refactored encryption.less to dataDisplayTable.less to use it for both
    the fibonacci page and the angularAes page.

--- js/controllers/MacroController.js
+++ js/controllers/MacroController.js
@@ -1,18 +1,54 @@
 angular.module("app").controller('MacroCtrl', function($scope) {
     $scope.bodyWeight;
     $scope.caloricMaintenance;
-    $scope.calculateMacros = function(){
-//        maintenance+=maintenanceModifier;
-        $scope.protein = $scope.bodyWeight;
-        $scope.proCals = $scope.protein*4; //4 cals per gram of protein
-        
-        $scope.fats = $scope.bodyWeight*.45;
-        $scope.fatCals = $scope.fats*9;               //9 cals per gram of protein
-        $scope.carbCals = $scope.caloricMaintenance - ($scope.proCals+$scope.fatCals);
-        $scope.carbs = $scope.carbCals/4;            //4 cals per gram of carb 
+    $scope.protein = {
+        val: 0,
+        cals: 0,
+        mod: 1,
+        calsPerGram: 4
+    };
+    $scope.fats = {
+        val: 0,
+        cals: 0,
+        mod: 45,
+        calsPerGram: 9
+    };
+    $scope.carbs = {
+        val: 0,
+        cals: 0,
+        mod: 0,
+        calsPerGram: 4
+    };
+    
+    $scope.calculateMacros = function () {
+        $scope.calculateProtein();
+        $scope.calculateFats();
+        $scope.calculateCarbs();
+    };
+
+    $scope.calculateProtein = function () {
+        $scope.protein.val = $scope.bodyWeight * $scope.protein.mod;
+        $scope.protein.cals = $scope.protein.val * $scope.protein.calsPerGram;
         
         console.log("Protein: " + $scope.protein);
-        console.log("Carbs: "   + $scope.carbs);
-        console.log("Fats: "    + $scope.fats);
-    }
+    };
+    $scope.calculateFats = function () {
+        console.log("Fat Slider Val: " + $scope.fats.mod / 100);
+        
+        var fatGrams = $scope.bodyWeight * ($scope.fats.mod/100);
+        $scope.fats.val = Math.round(fatGrams*1000)/1000;
+        
+        var fatCals = $scope.fats.val * $scope.fats.calsPerGram;
+        $scope.fats.cals = Math.round(fatCals*1000)/1000;
+        
+        console.log("Carbs: " + $scope.carbs);
+    };
+    $scope.calculateCarbs = function () {
+        var carbCals = $scope.caloricMaintenance - ($scope.protein.cals + $scope.fats.cals);
+        $scope.carbs.cals = Math.round(carbCals * 1000) / 1000;
+        
+        $scope.carbs.val = $scope.carbs.cals / $scope.carbs.calsPerGram;
+        
+        console.log("Fats: " + $scope.fats);
+    };
 });
